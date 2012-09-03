d34a4973497bf8cc3ad6951360b0f38cfb01d5ed
Ubuntu <ubuntu@ip-10-122-237-73.ec2.internal>
2012-09-03 23:55:54 +0000

    speech and noise are the same thing

--- etc/schema.sql
+++ etc/schema.sql
@@ -20,19 +20,13 @@ CREATE TABLE IF NOT EXISTS quota (
     duplicity DOUBLE NOT NULL DEFAULT 1
 );
 CREATE TABLE IF NOT EXISTS speech (
-    access VARBINARY(32) NOT NULL,
+    source VARBINARY(32) NOT NULL,
+    target VARBINARY(32) NOT NULL,
     intent VARBINARY(32) NOT NULL,
     active BIT NOT NULL DEFAULT 1,
     content TEXT NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    KEY(intent,active,access),
-    KEY(access,active,intent)
-);
-CREATE TABLE IF NOT EXISTS noise (
-    target VARBINARY(32) NOT NULL,
-    access VARBINARY(32) NOT NULL,
-    active BIT NOT NULL DEFAULT 1,
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    KEY(target,active,access),
-    KEY(access,active,target)
+    KEY(source,active),
+    KEY(target,active),
+    KEY(intent,active)
 );
